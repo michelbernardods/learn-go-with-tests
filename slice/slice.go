94a3ea9cf58c0399c024d8b4150364e06827a85a
Michel Bernardo <michel.bernardo@bemobi.com.br>
2021-02-27 19:17:22 -0300

    Test Sum with array

---
+++ slice/slice.go
@@ -0,0 +1,15 @@
+package main
+
+func Sum(numbers [5]int) int {
+	total := 0
+
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total
+}
+
+func main() {
+
+}
---
+++ slice/slice_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestSumSlice(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	result := Sum(numbers)
+	expected := 15
+
+	if result != expected {
+		t.Errorf("expeted '%d' but result is '%d'. Numbers = %v", expected, result, numbers)
+	}
+}
