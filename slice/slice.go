e0a5b12b9e5db30b3b806ede3e23ab5a1aeb9160
Michel Bernardo <michel.bernardo@bemobi.com.br>
2021-02-27 19:49:09 -0300

    Refactoring function 'SumSlice'

--- slice/slice.go
+++ slice/slice.go
@@ -10,15 +10,13 @@ func Sum(numbers []int) int {
 	return total
 }
 
-func SumSlice(numbersToSum ...[]int) (sum []int) {
-	numberOfNumbers := len(numbersToSum)
-	sum = make([]int, numberOfNumbers)
-
-	for i, number := range numbersToSum {
-		sum[i] = Sum(number)
+func SumSlice(numbersToSum ...[]int) []int {
+	var sum []int
+	for _, numbers := range numbersToSum {
+		sum = append(sum, Sum(numbers))
 	}
 
-	return
+	return sum
 }
 
 func main() {
